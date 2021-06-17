4c2d322191aed66403a1d6fe0ead8bcbd95a922e
cbreusing <77281303+cbreusing@users.noreply.github.com>
2021-06-17 06:45:35 -0400

    Add files via upload

--- SeparateMetagenomicReads.sh
+++ SeparateMetagenomicReads.sh
@@ -15,9 +15,9 @@ module load bbmap
 for file in `cat filelist.txt`
 do
 
-java -jar /gpfs/data/rbeinart/bin/trinityrnaseq-v2.11.0/trinity-plugins/Trimmomatic/trimmomatic-0.36.jar PE -threads 50 -phred33 /gpfs/data/rbeinart/Raw_reads/Snail_metagenomics_UCD/${file}_*R1_001.fastq.gz /gpfs/data/rbeinart/Raw_reads/Snail_metagenomics_UCD/${file}_*R2_001.fastq.gz ${file}_R1_paired.fq ${file}_R1_unpaired.fq ${file}_R2_paired.fq ${file}_R2_unpaired.fq ILLUMINACLIP:/gpfs/data/rbeinart/cbreusing/Adapters/Tn5.fasta:2:30:10 SLIDINGWINDOW:4:20 LEADING:5 TRAILING:5 MINLEN:75
-bowtie2 -p 50 -x /gpfs/data/rbeinart/Databases/contaminants -1 ${file}_R1_paired.fq -2 ${file}_R2_paired.fq | samtools view -bS -h -@ 50 - | samtools sort -@ 50 - > ${file}.bowtie2.cont.sorted.bam
-samtools view -@ 50 -f12 ${file}.bowtie2.cont.sorted.bam > ${file}.cont.unmapped.sam
+java -jar /gpfs/data/rbeinart/bin/trinityrnaseq-v2.11.0/trinity-plugins/Trimmomatic/trimmomatic-0.36.jar PE -threads 24 -phred33 /gpfs/data/rbeinart/Raw_reads/Snail_metagenomics_UCD/${file}_*R1_001.fastq.gz /gpfs/data/rbeinart/Raw_reads/Snail_metagenomics_UCD/${file}_*R2_001.fastq.gz ${file}_R1_paired.fq ${file}_R1_unpaired.fq ${file}_R2_paired.fq ${file}_R2_unpaired.fq ILLUMINACLIP:/gpfs/data/rbeinart/cbreusing/Adapters/Tn5.fasta:2:30:10 SLIDINGWINDOW:4:20 LEADING:5 TRAILING:5 MINLEN:75
+bowtie2 -p 24 -x /gpfs/data/rbeinart/Databases/contaminants -1 ${file}_R1_paired.fq -2 ${file}_R2_paired.fq | samtools view -bS -h -@ 24 - | samtools sort -@ 24 - > ${file}.bowtie2.cont.sorted.bam
+samtools view -@ 24 -f12 ${file}.bowtie2.cont.sorted.bam > ${file}.cont.unmapped.sam
 cut -f1 ${file}.cont.unmapped.sam | sort | uniq > ${file}.cont.unmapped_ids.lst
 seqtk subseq ${file}_R1_paired.fq ${file}.cont.unmapped_ids.lst > ${file}_R1_clean.fastq
 seqtk subseq ${file}_R2_paired.fq ${file}.cont.unmapped_ids.lst > ${file}_R2_clean.fastq
